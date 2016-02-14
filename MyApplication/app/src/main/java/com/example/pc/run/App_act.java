87b7e01c8b98e99aa984479c81837b505b8423f1
Joss Miller-Todd <joss.miller-todd@kcl.ac.uk>
2016-02-14 18:43:51 +0000

    no message

--- MyApplication/app/src/main/java/com/example/pc/run/App_act.java
+++ MyApplication/app/src/main/java/com/example/pc/run/App_act.java
@@ -4,16 +4,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.pc.run.Listeners.OnSwipeTouchListener;
+
 public class App_act extends AppCompatActivity {
 
-    Button chatBtn;
-
-
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_act);
 
-        chatBtn = (Button) findViewById(R.id.enterChat);
+        img = (ImageView)findViewById(R.id.imageView);
+
+        /*
+        Basic overriding for implementation - to be implemented for profile views
+         */
+        img.setOnTouchListener(new OnSwipeTouchListener(App_act.this){
+
+            public void onSwipeTop(){
+                img.setImageResource(R.drawable.fiveimg);
+            }
+            public void onSwipeRight(){
+                img.setImageResource(R.drawable.fourimg);
+            }
+            public void onSwipeLeft(){
+                img.setImageResource(R.drawable.twoimg);
+            }
+            public void onSwipeBottom(){
+                img.setImageResource(R.drawable.threeimg);
+            }
+        });
     }
 }
+
